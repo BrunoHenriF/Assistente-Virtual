2dc2e69a97278a4a35f5bb40f91314c50aa8f243
Bruno_ferreira <bruno_hferreira@outlook.com>
2021-07-07 02:28:57 -0300

    mqtt não funcionando

--- rossi.py
+++ rossi.py
@@ -4,14 +4,21 @@ from playsound import playsound
 from requests import get
 from bs4 import BeautifulSoup
 from paho.mqtt import publish
+import paho.mqtt.client as mqtt
 
-
+def cria_audio(comando):
+    
+    tts = gTTS(comando, lang='pt-br')
+    tts.save('./audios/comando.mp3')
+    playsound('./audios/comando.mp3')
+    
 palavra_chave = 'rossi'
 
 def monitora_audio():
     
     microfone = sr.Recognizer()
     with sr.Microphone() as source:
+        
         while True:
             
             print("ROSSI: Aguardando comandos...")
@@ -21,19 +28,12 @@ def monitora_audio():
             trigger = trigger.lower()
         
             if palavra_chave in trigger:
+                
                 print("COMANDO: " + trigger)
                 playsound('./audios/javou.mp3')
                 executa_comandos(trigger)
                 break
 
-
-
-def cria_audio(comando):
-    
-    tts = gTTS(comando, lang='pt-br')
-    tts.save('./audios/comando.mp3')
-    playsound('./audios/comando.mp3')
-    
     
 def executa_comandos(trigger):
     
@@ -61,12 +61,11 @@ def previsao_tempo():
     pass        
 
 
-def publica_mqtt(topic, payload):
-    publish.single(topic, payload=payload, qos=1, retain=True, hostname='vo ve ainda', port='numsei', client_id='rossi', auth={'username': '', 'password': ''})
-
-
-def main():
-    monitora_audio()    
-
-
-main()
+def publica_mqtt():
+    mqttc = mqtt.Client()
+    mqttc.connect("iot.eclipse.org", 1883)
+    mqttc.publish("rossi/status", '1')
+    mqttc.loop(2)
+    
+    # publish.single(topic, payload=payload, qos=1, retain=True, hostname='85dd1517ad9c4248a66685c3f8fe2b51.s1.eu.hivemq.cloud', 
+    #                port=8883, client_id='rossi', auth={'username': 'rossi', 'password': 'Rossi123'})
